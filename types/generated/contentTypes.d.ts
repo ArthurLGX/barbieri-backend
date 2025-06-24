615f2ba841fd90f1c708dc46ba8300fedf45efc7
Arthur Le_Goux <arthur.legoux@gmail.com>
2025-06-24 16:43:21 +0200

    add of ville into References

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -472,6 +472,7 @@ export interface ApiReferenceReference extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    ville: Schema.Attribute.String;
   };
 }
 
